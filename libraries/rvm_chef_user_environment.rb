04ed93dce1239c831639ebb53f8a3ba33a8e7237
Matt Edlefsen <matt@xforty.com>
2013-03-14 07:43:46 -0700

    Fix per-user rvm install
    Was getting the wrong rvm path because RVM gem uses the current user
    (which is root).  Force setting 'rvm_path' environment variable to
    correct value so that we get the correct value.

--- libraries/rvm_chef_user_environment.rb
+++ libraries/rvm_chef_user_environment.rb
@@ -23,17 +23,22 @@ def create_rvm_chef_user_environment
   klass = Class.new(::RVM::Environment) do
     attr_reader :user, :source_environment
 
+    def rvm(*args)
+      ENV['rvm_path'] = @rvm_path
+      super
+    end
+
     def initialize(user = nil, environment_name = "default", options = {})
       @source_environment = options.delete(:source_environment)
       @source_environment = true if @source_environment.nil?
       @user = user
       # explicitly set rvm_path if user is set
       if @user.nil?
-        config['rvm_path'] = @@root_rvm_path
+        @rvm_path = @@root_rvm_path
       else
-        config['rvm_path'] = File.join(Etc.getpwnam(@user).dir, '.rvm')
+        @rvm_path = File.join(Etc.getpwnam(@user).dir, '.rvm')
       end
-
+      config['rvm_path'] = @rvm_path
       merge_config! options
       @environment_name = environment_name
       @shell_wrapper = ::RVM::Shell::ChefWrapper.new(@user)
